c2e0b8ae2cc051e7aea00b9f58188e16ebfb1dd2
Li Lin <li.lin@hometrack.com.au>
2019-01-14 14:02:52 +0800

    Config server and build

---
+++ server.js
@@ -0,0 +1,20 @@
+const Express = require('express');
+const path = require('path');
+
+const app = new Express();
+const port = 3000;
+
+app.disable('x-powered-by');
+app.use('/', Express.static(path.join(process.cwd(), 'dist'), { maxAge: '1d' }));
+app.use('/node_modules', Express.static(__dirname + '/node_modules'));
+// Make static assets stay at browser for 24h
+app.use(function(req, res, next) {
+  res.set({ 'Cache-Control': 'public, max-age=86400' });
+  next();
+});
+
+app.listen(port, function() {
+  const message = `Server listening on port:${port}`;
+
+  console.log(message);
+});
